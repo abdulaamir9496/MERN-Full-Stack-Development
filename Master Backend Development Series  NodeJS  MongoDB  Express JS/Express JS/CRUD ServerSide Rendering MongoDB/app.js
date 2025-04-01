c1c63df7f700ffe0d8a35746eac56611fce84503
Abdul Aamir <abdulaamir9496@gmail.com>
2025-04-02 00:11:08 +0530

    CRUD Operations with EJS & Server-Side Rendering | MongoDB |

--- Master Backend Development Series  NodeJS  MongoDB  Express JS/Express JS/CRUD ServerSide Rendering MongoDB/app.js
+++ Master Backend Development Series  NodeJS  MongoDB  Express JS/Express JS/CRUD ServerSide Rendering MongoDB/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const userModel = require('./models/user');
+const user = require('./models/user');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -8,7 +10,49 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.send('Welcome to CRUD Operations Server-Side Rendering using MongoDB and Express');
+    res.render('index');
+});
+
+app.get('/read', async (req, res) => {
+    let users = await userModel.find();
+    res.render('read', { users });
+});
+
+app.post('/create', async (req, res) => {
+    // console.log(req.body);
+    // res.send('Data received');
+
+    // const user = new userModel({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     image: req.body.image
+    // });
+
+    let { name, email, image } = req.body;   //destructuring
+    let createdUser = await userModel.create({
+        // name: name,
+        name,  //if name is same as key name then we can write like this also in es6 syntax.
+        email,
+        image
+    });
+    // res.send(createdUser);
+    res.redirect('/read');
+});
+
+app.get('/edit/:userid', async (req, res) => {
+    let user = await userModel.findOne({_id: req.params.userid});
+    res.render('edit', { user });
+});
+
+app.get('/delete/:id', async (req, res) => {
+    let users = await userModel.findByIdAndDelete({ _id: req.params.id});
+    res.redirect('/read');
+});
+
+app.post('/update/:userid', async (req, res) => {
+    let { name, email, image } = req.body;
+    await userModel.findByIdAndUpdate(req.params.userid, { name, email, image });
+    res.redirect('/read');
 });
 
 app.listen(3000);
