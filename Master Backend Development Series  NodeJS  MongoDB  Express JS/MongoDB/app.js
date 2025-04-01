b08aa29b21c977fb8b767b4fa92182c6d3f8d3f1
Abdul Aamir <abdulaamir9496@gmail.com>
2025-04-01 16:36:33 +0530

    Master CRUD Operations with MongoDB

--- Master Backend Development Series  NodeJS  MongoDB  Express JS/MongoDB/app.js
+++ Master Backend Development Series  NodeJS  MongoDB  Express JS/MongoDB/app.js
@@ -1,8 +1,80 @@
 const express = require('express');
 const app = express();
 
+//for performing CRUD operations
+const User = require('./userModel');
+
+app.use(express.json()); // To handle JSON requests
+
 app.get('/', (req, res) => {
     res.send('Code for the world!');
 });
 
-app.listen(3000);
+//MongoDB code is asynchronous: said by mongoDB to make run code line by line we use async and await.
+//It takes some time to connect to the database
+//So, we use callbacks to handle the response
+//If the connection is successful, the callback function is called with the response
+//If the connection is unsuccessful, the callback function is called with the error
+// Create User
+app.get('/create', async (req, res) => {
+    let createduser = await User.create({
+        name: 'Aamir',
+        email: 'aamir@example.com',
+        userName: 'aamir123'  // Ensure consistent field name
+    });
+    res.send(createduser);
+});
+
+//We can read all the created users from the database
+// Read Users
+app.get('/read', async (req, res) => {
+    let users = await User.find();     
+    //find gives us array, & findOne gives us object, 
+    // & findById gives us object by id, & 
+    // findByIdAndDelete gives us object by id and delete it, & 
+    // deleteMany gives us all the objects and delete them all, & 
+    // findOneAndUpdate gives us object by id and update it, & 
+    // findOneAndDelete gives us object by id and delete it, & 
+    // findOneAndRemove gives us object by id and remove it, & 
+    // updateMany gives us all the objects and update them all, & 
+    // updateOne gives us one object and update it, & 
+    res.send(users);
+}); 
+
+//We can also read a specific user from the database
+// app.get('/read/:id', async (req, res) => {
+//     let users = await userModel.findById(req.params.id);
+//     res.send(user);
+// });
+
+// Update User
+app.get('/update', async (req, res) => {
+    let updatedUser = await User.findOneAndUpdate(
+        {userName: "aamir123"}, 
+        {name: "Muhammad"}, 
+        {new: true}
+    );
+    res.send(updatedUser);
+});
+
+// Delete User
+app.get('/delete', async (req, res) => {
+    let deletedUser = await User.findOneAndDelete({userName: "aamir123"});
+    res.send(deletedUser);
+});
+
+//We can delete a specific user from the database
+// app.get('/delete/:id', async (req, res) => {
+//     let users = await User.findByIdAndDelete(req.params.id);
+//     res.send(users);
+// });
+
+//We can delete all the users from the database
+// app.get('/deleteAll', async (req, res) => {
+//     let users = await User.deleteMany();
+//     res.send(users);
+// });
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
--- Master Backend Development Series  NodeJS  MongoDB  Express JS/MongoDB/usermodel.js
+++ Master Backend Development Series  NodeJS  MongoDB  Express JS/MongoDB/usermodel.js
@@ -1,3 +1,22 @@
 const mongoose = require('mongoose');
 
-mongoose.mongoURI = 'mongodb://localhost:27017/CodeForTheWorld';
+mongoose.connect("mongodb://localhost:27017/mongopractice");
+
+//Schema for the user model, which is the blueprint of the user object that will be stored in the database
+// Define User Schema
+const userSchema = mongoose.Schema({
+    name: String,
+    userName: String,   // Ensure correct spelling (capital "N")
+    email: String
+});
+
+//Model for the user schema that will be used to interact with the database collection of users 
+//On the basis of Model we do CRUD operations
+// Create and Export User Model
+module.exports = mongoose.model("User", userSchema);
+
+//Exporting the mongoose object so that it can be used in other files
+// module.exports = mongoose;
+// module.exports = mongoose.model('user', userSchema);
+
+// mongoose.mongoURI = 'mongodb://localhost:27017/CodeForTheWorld';
